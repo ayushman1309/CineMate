5d14ed9e3adf0a6c26a897275cffe853bf44a25d
ayushman1309 <pathakayush1309@gmail.com>
2025-09-24 21:16:32 +0530

    update server.js

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ import connectDB from "./config/db.js";
 import { serve } from "inngest/express";
 import { clerkMiddleware } from "@clerk/express";
 import { functions, inngest } from "./inngest/index.js";
+import rawBody from "body-parser"; // add this
 
 const app = express();
 const port = 3000;
@@ -13,15 +14,19 @@ const port = 3000;
 await connectDB();
 
 // Middleware
-app.use(express.json());
 app.use(cors());
 app.use(clerkMiddleware());
+app.use(express.json()); // only for non-Inngest routes
 
 // API Routes
 app.get("/", (req, res) => res.send("Server is Live!!"));
 
-// Inngest route
-app.use("/api/inngest", serve({ client: inngest, functions }));
+// Inngest route (with raw body)
+app.post(
+  "/api/inngest",
+  rawBody.raw({ type: "*/*" }), // important for signature verification
+  serve({ client: inngest, functions })
+);
 
 // Start server
 app.listen(port, () =>
