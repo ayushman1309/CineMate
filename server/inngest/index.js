ff940f670b0513ae8e1c3c940cac01a5bc0f01da
ayushman1309 <pathakayush1309@gmail.com>
2025-09-24 21:01:43 +0530

    Connect Inngest with Clerk

---
+++ server/inngest/index.js
@@ -0,0 +1,158 @@
+import { Inngest } from "inngest"
+
+//create a client to send and receive events
+
+export const inngest = new Inngest({
+  id:"movie-ticket-booking"
+});
+
+// Inngest function to save user data to a database
+const syncUserCreation = inngest.createFunction(
+  { 
+    id: "sync-user-from-clerk" 
+  },
+  { 
+    event: "clerk/user.created" 
+  },
+  async ({ event }) => {
+    const { id, first_name, last_name, email_addresses, image_url } =
+      event.data;
+    const userData = {
+      _id: id,
+      email: email_addresses[0].email_address,
+      name: first_name + " " + last_name,
+      image: image_url,
+    };
+    await User.create(userData);
+  }
+);
+// Inngest function to delete user data from database
+const syncUserDeletion = inngest.createFunction(
+  { id: "delete-user-with-clerk" },
+  { event: "clerk/user.deleted" },
+  async ({ event }) => {
+    const { id } = event.data;
+    await User.findByIdAndDelete(id);
+  }
+);
+
+// Inngest function to update user data in database
+const syncUserUpdation = inngest.createFunction(
+  { id: "update-user-from-clerk" },
+  { event: "clerk/user.updated" },
+  async ({ event }) => {
+    const { id, first_name, last_name, email_addresses, image_url } =
+      event.data;
+    const userData = {
+      _id: id,
+      email: email_addresses[0].email_address,
+      name: first_name + " " + last_name,
+      image: image_url,
+    };
+    await User.findByIdAndUpdate(id, userData);
+  }
+);
+
+// Inngest function to cancel booking and release seats of show after 10 min of booking created if the payment is not done.
+const releaseSeatsAndDeleteBooking = inngest.createFunction(
+  { id: "release-seats-delete-booking" },
+  { event: "app/checkpayment" },
+  async ({ event, step }) => {
+    const tenMinutesLater = new Date(Date.now() + 10 * 60 * 1000);
+    await step.sleepUntil("wait-for-10-minutes", tenMinutesLater);
+
+    await step.run("check-payment-status", async () => {
+      const bookingId = event.data.bookingId;
+      const booking = await Booking.findById(bookingId);
+
+      // If payment is not made, release seats and delete booking
+      if (!booking.isPaid) {
+        const show = await Show.findById(booking.show);
+        booking.bookedSeats.forEach((seat) => {
+          delete show.occupiedSeats[seat];
+        });
+        show.markModified("occupiedSeats");
+        await show.save();
+        await Booking.findByIdAndDelete(booking._id);
+      }
+    });
+  }
+);
+
+// Inngest function to send email when user books a show
+const sendBookingConfirmationEmail = inngest.createFunction(
+  { id: "send-booking-confirmation-email" },
+  { event: "app/show.booked" },
+  async ({ event, step }) => {
+    const { bookingId } = event.data;
+
+    const booking = await Booking.findById(bookingId)
+      .populate({
+        path: "show",
+        populate: { path: "movie", model: "Movie" },
+      })
+      .populate("user");
+
+    await sendEmail({
+      to: booking.user.email,
+      subject: `Payment Confirmation: "${booking.show.movie.title}" booked !`,
+      body: `<div style="font-family: Arial, sans-serif; line-height: 1.5;">
+                   <h2>Hi ${booking.user.name},</h2>
+                   <p>Your booking for <strong style="color: #F84565;">"${
+                     booking.show.movie.title
+                   }"</strong> is confirmed.</p>
+                   <p>
+                      <strong>Date:</strong> ${new Date(
+                        booking.show.showDateTime
+                      ).toLocaleDateString("en-US", {
+                        timeZone: "Asia/Kolkata",
+                      })}<br/>
+                      <strong>Time:</strong> ${new Date(
+                        booking.show.showDateTime
+                      ).toLocaleTimeString("en-US", {
+                        timeZone: "Asia/Kolkata",
+                      })}
+                  </p>
+                  <p>Enjoy the show! 🍿</p>
+                  <p>Thanks for booking with us!<br/>- QuickShow Team</p>
+             </div>`,
+    });
+  }
+);
+
+// Inngest function to send notifications when a new show is Added
+const sendNewShowNotification = inngest.createFunction(
+  { id: "send-new-show-notification" },
+  { event: "app/show.added" },
+  async ({ event }) => {
+    const { movieTitle } = event.data;
+
+    const users = await User.find({});
+
+    for (const user of users) {
+      const userEmail = user.email;
+      const userName = user.name;
+
+      const subject = `🎞️ New Show Added: ${movieTitle}`;
+      const body = `<div>
+                      <h2>${userName}</h2>
+                    </div>`;
+
+      await sendEmail({
+        to: userEmail,
+        subject,
+        body,
+      });
+    }
+    return { message: "Notification sent." };
+  }
+);
+
+export const functions = [
+  syncUserCreation,
+  syncUserDeletion,
+  syncUserUpdation,
+  releaseSeatsAndDeleteBooking,
+  sendBookingConfirmationEmail,
+  sendNewShowNotification,
+];
--- server/server.js
+++ server/server.js
@@ -4,7 +4,9 @@ import cors from "cors"
 
 import "dotenv/config"
 import connectDB from "./config/db.js";
+import { serve } from "inngest/express"
 import { clerkMiddleware } from '@clerk/express'
+import { functions, inngest } from "./inngest/index.js";
 
 const app = express();
 const port = 3000;
@@ -20,6 +22,7 @@ app.use(clerkMiddleware())
 
 //API ROUTE
 app.get('/' , (req , res) => res.send("Server is Live!!"))
+app.use('/api/inngest' , ser({ client : inngest , functions}))
 
 app.listen(port , () => console.log(`Server Listening at http://localhost:${port}`)
 );
