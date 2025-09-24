2823ef48650db86d015456ee860a838400ca4dcc
ayushman1309 <pathakayush1309@gmail.com>
2025-09-24 21:04:40 +0530

    update server

--- server/server.js
+++ server/server.js
@@ -1,28 +1,29 @@
-import express from "express"
-
-import cors from "cors"
-
-import "dotenv/config"
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
 import connectDB from "./config/db.js";
-import { serve } from "inngest/express"
-import { clerkMiddleware } from '@clerk/express'
+import { serve } from "inngest/express";
+import { clerkMiddleware } from "@clerk/express";
 import { functions, inngest } from "./inngest/index.js";
 
 const app = express();
 const port = 3000;
 
+// Connect DB
+await connectDB();
 
-await connectDB()
-//middleWare
+// Middleware
+app.use(express.json());
+app.use(cors());
+app.use(clerkMiddleware());
 
-app.use(express.json())
-app.use(cors())
-app.use(clerkMiddleware())
+// API Routes
+app.get("/", (req, res) => res.send("Server is Live!!"));
 
+// Inngest route
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
-//API ROUTE
-app.get('/' , (req , res) => res.send("Server is Live!!"))
-app.use('/api/inngest' , ser({ client : inngest , functions}))
-
-app.listen(port , () => console.log(`Server Listening at http://localhost:${port}`)
+// Start server
+app.listen(port, () =>
+  console.log(`Server Listening at http://localhost:${port}`)
 );
