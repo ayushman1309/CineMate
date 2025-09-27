af971ae0c6024f51b0c757c2d28d9260d3b223f9
ayushman1309 <pathakayush1309@gmail.com>
2025-09-28 02:16:58 +0530

    ...

--- client/src/pages/Admin/Layout.jsx
+++ client/src/pages/Admin/Layout.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import AdminNavbar from "../../components/admin/AdminNavbar.jsx";
+import AdminNavbar from "../../components/Admin/AdminNavbar.jsx";
 import AdminSidebar from "../../components/admin/AdminSideBar.jsx";
 import { Outlet } from "react-router-dom";
 import { useAppContext } from "../../context/AppContext";
