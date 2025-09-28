a3fd18c40d270ffaa8e5bbea11dee1c1629726d3
ayushman1309 <pathakayush1309@gmail.com>
2025-09-28 12:19:17 +0530

    Update NavBar

--- client/src/components/admin/AdminNavbar.jsx
+++ client/src/components/admin/AdminNavbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import logo from '../../assets/logo.png'
 
-const AdminNavbar = () => {
+const AdminNavBar = () => {
   return (
     <div className='flex items-center justify-start px-6 md:px-10 h-16 border-b border-gray-300/30'>
       <Link to='/' className='flex items-center'>
@@ -12,4 +12,4 @@ const AdminNavbar = () => {
   )
 }
 
-export default AdminNavbar
+export default AdminNavBar
--- client/src/pages/admin/Layout.jsx
+++ client/src/pages/admin/Layout.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import AdminNavbar from "../../components/admin/AdminNavbar";
+import AdminNavBar from "../../components/admin/AdminNavBar";
 import AdminSideBar from "../../components/admin/AdminSideBar";
 import { Outlet } from "react-router-dom";
 import { useAppContext } from "../../context/AppContext";
@@ -13,9 +13,9 @@ const Layout = () => {
   }, []);
   return isAdmin ? (
     <>
-      <AdminNavbar />
+      <AdminNavBar />
       <div className="flex">
-        <AdminSidebar />
+        <AdminSideBar />
         <div className="flex-1 px-4 py-10 md:px-10 h-[calc(100vh-64px)] overflow-y-auto">
           <Outlet />
         </div>
